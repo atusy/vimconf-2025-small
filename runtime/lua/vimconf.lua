191611dc7270a67bad8860dd614e2b01d4134a93
atusy <30277794+atusy@users.noreply.github.com>
2025-10-24 00:53:10 +0900

    feat: configure fold

--- runtime/lua/vimconf.lua
+++ runtime/lua/vimconf.lua
@@ -7,6 +7,11 @@ vim.api.nvim_set_hl(0, "@string.special.url.uri", { underline = true })
 vim.api.nvim_set_hl(0, "@string.special.url.path.uri", { reverse = true })
 vim.api.nvim_set_hl(0, "@string.special.url.path.python", { reverse = true })
 
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.o.foldtext = ""
+vim.o.foldlevel = 99
+
 local enable_tsnode_marker = true
 
 if not enable_tsnode_marker then
